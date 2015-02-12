32f9b9daf1ec2586e88dae1bb148d3b7c544a0e3
Eric O'Connell <eric.oconnell@idealist.org>
2015-02-11 17:04:53 -0800

    Fix derped webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,36 +2,6 @@ var chalk = require('chalk');
 var webpack = require('webpack');
 
 
-module.exports = {
-    context: __dirname + '/app',
-    entry: {
-        vendor: env('vendor'),
-        app: './client.jsx'
-    },
-    output: {
-        filename: 'client.js',
-        path: __dirname + '/dist',
-        publicPath: '/js'
-    },
-    devtool: 'eval',
-    plugins: env('plugins'),
-    resolve: {
-        // Look directly in app dir for modules
-        root: __dirname + '/app',
-        // Allow to omit extensions when requiring these files
-        extensions: ['', '.js', '.jsx']
-    },
-    module: {
-        loaders: [
-            { test: /\.js$/, exclude: /node_modules/, loaders: ['6to5?sourceMap&experimental'] },
-            // Pass *.jsx files through jsx-loader transform
-            { test: /\.jsx$/, loaders: ['react-hot', '6to5?sourceMap&experimental', 'jsx'] },
-            { test: /\.css$/, loaders: ['style', 'css'] }
-        ]
-    }
-}
-
-
 var environments = {
     production: {
         vendor: ['react', 'react-router'],
@@ -61,17 +31,49 @@ var environments = {
 };
 
 
+module.exports = {
+    context: __dirname + '/app',
+    entry: {
+        // output 2 bundles of javascript, one for vendor, one for application
+        vendor: env('vendor'),
+        app: './client.jsx'
+    },
+    output: {
+        filename: 'client.js',
+        path: __dirname + '/dist',
+        publicPath: '/js'
+    },
+    devtool: 'eval',
+    plugins: env('plugins'),
+    resolve: {
+        // Look directly in app dir for modules
+        root: __dirname + '/app',
+        // Allow to omit extensions when requiring these files
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, exclude: /node_modules/, loaders: ['6to5?sourceMap&experimental'] },
+            // Pass *.jsx files through jsx-loader transform
+            { test: /\.jsx$/, loaders: ['react-hot', '6to5?sourceMap&experimental', 'jsx'] },
+            { test: /\.css$/, loaders: ['style', 'css'] }
+        ]
+    }
+}
+
+
 function env(key) {
     var result = undefined;
     try {
         var env = process.env.NODE_ENV || 'development';
         result = environments[env][key];
         if (result === undefined) {
-            console.warn(chalk.yellow('Undefined environment key %s in %s', key, env));
+            console.warn(chalk.yellow('Undefined environment key', key, ' in ', env));
         }
         return result;
     } catch(e) {
-        console.error(chalk.red('Exception occurred retrieving environment config key %s in %s', key, env));
+        console.error(chalk.red('Exception occurred retrieving environment config key', key, ' in ', env));
+        console.error(e.stack);
         process.exit(1);
     }
 }
